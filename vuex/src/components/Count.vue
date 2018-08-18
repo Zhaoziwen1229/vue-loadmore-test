dee59b483ff126a8ec4bf365c7970fa878f0c593
itw_zhaozw01 <itw_zhaozw01@tkmail.com>
2018-08-18 15:18:15 +0800

    VUEX项目提交

---
+++ vuex/src/components/Count.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        {{msg}}
+        <hr/>
+        <h3>{{$store.state.count}}</h3>
+        <div>
+             <button @click="$store.commit('add')">+</button>
+             <button @click="$store.commit('reduce')">-</button>
+        </div>
+    </div>
+   
+</template>
+<script>
+import store from "@/vuex/store"
+export default {
+    data (){
+        return {
+            msg:'Hello Vuex'
+        }
+    },
+    store
+
+}
+</script>
+<style scoped>
+
+</style>
